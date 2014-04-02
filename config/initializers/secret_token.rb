a183ee7cea9787871e9b83b939c55c011f1ef15d
klepa <klepa@klepa-n>
2014-04-02 16:15:38 +0400

    инициализация проекта. ref #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnkaaApp::Application.config.secret_key_base = 'c9aefd4c5f12f116d5868771bfc4853048e12764ee5b96730b0e0007d9fa5328ecc93fe30aa488bd95b016ebc7399797e59421720da4bcd477067cf9979fd766'
