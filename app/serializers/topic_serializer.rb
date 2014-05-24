b164628a113b84b33897285520a8a6b8b4eac015
Aleksey Zolotov <myxrome@gmail.com>
2014-05-24 13:19:45 +0400

    информация о Теме в api. resolve #4

--- app/serializers/topic_serializer.rb
+++ app/serializers/topic_serializer.rb
@@ -1,4 +1,6 @@
 class TopicSerializer < ActiveModel::Serializer
+  attributes :id, :name, :updated_at
+
   has_many :values, serializer: ValueShortSerializer
   has_many :categories, serializer: CategoryShortSerializer
 end
