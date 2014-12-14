90d34d76dd062fb5ae98d027d8b34434ee9a3a37
Aleksey Zolotov <myxrome@gmail.com>
2014-12-14 19:12:51 +0300

    исправление запроса, возвращает описание и промо корректно. добвлен аттрибут в запрос. ref #15

--- app/controllers/application_data_controller.rb
+++ app/controllers/application_data_controller.rb
@@ -2,12 +2,12 @@ class ApplicationDataController < ActionController::Base
 
   def index
     ActiveRecord::Base.establish_connection
-    result = ActiveRecord::Base.connection.execute lighting_query(params[:k], params[:u])
+    result = ActiveRecord::Base.connection.execute lighting_query(params[:k], params[:u], params[:q])
     render json: result.first['d'].to_s
   end
 
   private
-  def lighting_query(keys, date)
+  def lighting_query(keys, date, quality)
 "set work_mem = '16MB';
 with tg as (
     select tg.\"id\" as id, tg.\"order\" as o, tg.\"key\" as k, tg.\"name\" as n, tg.\"updated_at\" as u
@@ -49,19 +49,16 @@ d as (
 ),
 p as (
     select p.\"id\" as id, p.\"value_id\" as _id, p.\"order\" as o,
-    '/content/p/' || p.\"value_id\" || '/' || p.\"id\" || '/' || p.\"file_key\" || 'xhdpi' || '.jpg' as l,
+    '/content/p/' || p.\"value_id\" || '/' || p.\"id\" || '/' || p.\"file_key\" || '#{quality}' || '.jpg' as l,
     p.\"updated_at\" as u
     from promos p, _v v
     where p.\"value_id\" = v.\"id\"
 ),
 v as (
-    select v,
-    json_agg(row_to_json((d))) as d,
-    json_agg(row_to_json((p))) as p
-    from _v v, d d, p p
-    where d.\"_id\" = v.\"id\"
-    and p.\"_id\" = v.\"id\"
-    group by v
+    select _v.*,
+    ( select json_agg(row_to_json((d))) from d d where d.\"_id\" = _v.\"id\" ) as d,
+    ( select json_agg(row_to_json((p))) from p p where p.\"_id\" = _v.\"id\" ) as p
+    from _v _v
 ),
 ad as (
     select (select coalesce(json_agg(row_to_json((tg))), '[]') from tg where tg.\"u\" > '#{date}') as g,
